380d52a536b6976e01c685d299d4c24e80c08917
the-middle <middle.oo.oo.ak@gmail.com>
2021-04-27 00:19:20 +0300

    Major code refactor. Use the biggest image from infobox, skip icons and get text from almost all pages, I hope :D

--- bot_wiki_rand.py
+++ bot_wiki_rand.py
@@ -6,58 +6,51 @@ wiki_url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D
 get_url = requests.get(wiki_url)
 
 def getText():
-    if get_url.text.find("<p><b>") != -1:                                                       # Find if paragraph pattern present in string
-        stripped_string = re.findall(r'<p><b>.*[.</p>]', get_url.text)                          # Get this string
-        x = str(stripped_string)                                                                # Transform into string from an object
-        x = re.sub(r'<.*?>', '', x)                                                             # Delete all HTML tags
-        x = re.sub(r'&#\d*;', '', x)                                                            # Delete encoded special symbols
-        x = re.sub(r"[\[''\]]", '', x)                                                          # Delete [] and '' that was added while transforming to string
-        x = re.sub(r"(([А-я])(\d{1,3}))", r"\g<2>", x)     
-        return x
-    elif get_url.text.find("<p><i>") != -1:
-        stripped_string = re.findall(r'<p><i>.*[.</p>]', get_url.text)
-        x = str(stripped_string)
-        x = re.sub(r'<.*?>', '', x)
+    if get_url.text.find("<p>") != -1:
+        x = re.findall(r'<p>.*[.</p>]', get_url.text)
+        x = re.sub(r'<.*?>', '', x[0])
         x = re.sub(r'&#\d*;', '', x)
-        x = re.sub(r"[\[''\]]", '', x)  
-        x = re.sub(r"(([А-я])(\d{1,3}))", r"\g<2>", x)     
+        x = re.sub(r"(([А-я])(\d{1,3}))", r"\g<2>", x)
         return x
     else: 
-        #TODO: catch the <p>[symbol]<b> and <p> Text.
         return "Попалась неинтересная статья, повезет в следующий раз."
 
 def getURL():
-    link_str = str(re.findall(r'<link rel="canonical".*[.\/>]', get_url.text))                  # Get the needed part of a string
-    link_str = str(re.findall(r'\bhttps?://[^\s]+[^"/>]', link_str))                            # Get just url
-    link_str = re.sub(r"\['", '', link_str)                                                     # Delete brackets from str()
-    link_str = re.sub(r"\"/>\\'\]'\]", '', link_str)
+    link_str = re.findall(r'<link rel="canonical".*[.\/>]', get_url.text)
+    link_str = re.findall(r'\bhttps?://[^\s]+[^"/>]', link_str[0])
+    link_str = link_str[0]
     return link_str
 
 def getImg():
     if re.search(r'class="infobox-image"', get_url.text) != None:
-        link_img = str(re.findall(r'<table class="infobox.*<\/', get_url.text))
-        link_img = str(re.findall(r'<img.* />', link_img))
-        link_img = str(re.findall(r'src=".*"', link_img))
-        link_img = re.findall(r'upload.[^\s]*"', link_img)
-        if re.search(r'pictogram', link_img[0]) == None:
-            link_img = re.sub(r'"', '', link_img[0])
-            link_img = 'https://' + link_img
-            print(link_img)
-            return link_img
-        elif re.search(r'pictogram', link_img[1]) == None:
-            link_img = re.sub(r'"', '', link_img[1])
-            link_img = 'https://' + link_img
-            return link_img
-        elif re.search(r'pictogram', link_img[2]) == None:
-            link_img = re.sub(r'"', '', link_img[2])
-            link_img = 'https://' + link_img
-            return link_img
-        elif re.search(r'pictogram', link_img[3]) == None:
-            link_img = re.sub(r'"', '', link_img[3])
+        link_img = re.findall(r'class="infobox.*<\/', get_url.text)
+        link_img = re.findall(r'<img.* />', link_img[0])
+        link_img = re.findall(r'src=".*"', link_img[0])
+        if re.search(r'upload.[^\s]+ 2x', link_img[0]) != None:
+            link_img = re.findall(r'upload.[^\s]+ 2x', link_img[0])
+            link_img = re.sub(r' 2x', '', link_img[0])
             link_img = 'https://' + link_img
             return link_img
         else:
-            return ('Нет фото.')
+            if re.search(r'pictogram|mark', link_img[0]) == None:
+                link_img = re.sub(r'"', '', link_img[0])
+                link_img = 'https://' + link_img
+                print(link_img)
+                return link_img
+            elif re.search(r'pictogram|mark', link_img[1]) == None:
+                link_img = re.sub(r'"', '', link_img[1])
+                link_img = 'https://' + link_img
+                return link_img
+            elif re.search(r'pictogram|mark', link_img[2]) == None:
+                link_img = re.sub(r'"', '', link_img[2])
+                link_img = 'https://' + link_img
+                return link_img
+            elif re.search(r'pictogram|mark', link_img[3]) == None:
+                link_img = re.sub(r'"', '', link_img[3])
+                link_img = 'https://' + link_img
+                return link_img
+            else:
+                return ('Нет фото.')
     else:
         return('Нет фото.')
 
